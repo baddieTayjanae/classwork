48a900333ddfcdccaba1b1e0dd573a1cb5cbf0da
Tayjanae <tflowers@bostonk12.org>
2024-03-08 10:10:19 -0500

    Python Folder

--- HTML/Week_13/ 2024-03-06/ practice.py
+++ HTML/Week_13/ 2024-03-06/ practice.py
@@ -1,4 +1,4 @@
 #Tayjanae flowers
 #3/7/2024
-name=input("please enter your name:")
-print(f"Hi {name},I hope you are having an awsome day!")
+name=input("Please enter your name:")
+print(f"Hi {name}, I hope you are having an awsome day!")
---
+++ Python/Week_13/ practice2.py
@@ -0,0 +1,16 @@
+#Tayjanae
+#3/7/2024
+
+name=input("Please enter your name :")
+food=input(" Please enter your favorite food :")
+song=input("Please neter your favorie song :")
+quote=input("Please enter your favorite quote :")
+print(f"information about {name}")
+print("*****")
+print(f"Favorite food:{food}")
+print(f"Favorite song:{song}")
+print(f"Favorite quote:{quote}")
+print(f"{name}'s favorite food is {food}, their favorite song is \"{song}\" and their favorite quote is \"{quote}\".")
+
+
+
