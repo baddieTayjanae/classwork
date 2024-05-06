91837cb3a237f60ce552107e0628008c1d97ed7d
Tayjanae <tflowers@bostonk12.org>
2024-05-06 10:44:55 -0400

    List practice

---
+++ Python/week_17/2024-05-06/practice.py
@@ -0,0 +1,20 @@
+#Tayjanae flowers
+#2024-05-06
+
+numbers=[]
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+numbers.append(4)
+numbers.append(5)
+numbers.append(6)
+numbers.append(7)
+numbers.append(8)
+numbers.append(9)
+numbers.append(10)
+
+numbers2=[]
+for b in range(1,1001):
+    numbers2+=[b]
+print(numbers)
+print(numbers2)
