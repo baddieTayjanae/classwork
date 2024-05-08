15a6f62eeadfeec5ba8ed8eb5158650787f9c4e1
Tayjanae <tflowers@bostonk12.org>
2024-05-08 09:26:34 -0400

    List practice

---
+++ Python/week_17/check.py
@@ -0,0 +1,26 @@
+#Tayjanae flowers
+#5/7/2024
+
+#creating An empy list
+a=[]
+
+#using the operator
+a+=[1]
+a+=[2]
+a+=[3]
+
+#printing the list
+print(a)
+
+# creating a empty list named b
+b=[]
+
+#using the function to add to the list b
+b.append(1)
+b.append(2)
+b.append(3)
+#printing the list b
+print(b)
+
+
+
