befe8bbfdb5c7fb2943478d7b6ae9b91c5a69eb9
Tayjanae <tflowers@bostonk12.org>
2024-05-08 10:04:41 -0400

    List practice

---
+++ Python/week_20/final_exam/final.py
@@ -0,0 +1,16 @@
+def main():
+    print('Tayjanae')
+    dictionary=[]
+
+    with open('dictionary.txt','r') as f:
+        for line in f:
+            dictionary.append(line.strip())
+
+    print(dictionary)
+    dictionary.sort()
+    print("BIRD" in dictionary)
+    print("BEARD" in dictionary)
+    print("TECH"in dictionary)
+
+if __name__ == '__main__':
+    main()
