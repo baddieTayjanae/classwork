dbdfcdc17b5077d9fccda074f43348564941b604
Tayjanae <tflowers@bostonk12.org>
2024-05-08 10:04:41 -0400

    List practice

---
+++ Python/week_17/2024-05-08/lists.py
@@ -0,0 +1,12 @@
+#Tayjanae flowers
+#05/08/2024
+
+#creating a empty list
+lst=[]
+
+#using a forloop to add the numbers 1/100 to the list
+for b in range(1,100):
+    lst.append(b)
+
+#printing the list lst
+print(lst)
